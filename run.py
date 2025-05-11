38d1e876daed953b6dee8bda50e52d37b62534f5
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2025-05-11 00:00:15 +0000

    pytest

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
-
+from seed_data import seed_all
 app = create_app()
 
 if __name__ == '__main__':
+   
+    print("🌱 Seeding database...")
+    seed_all()
+    print("🚀 Starting Flask server...")
     app.run(debug=True)
